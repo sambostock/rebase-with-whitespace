38f4653fb8a7e106a563cd6e025ce185421bf46e
Sam Bostock <sam.bostock@shopify.com>
2019-03-26 17:29:24 -0400

    Nest ShopConversion in Launch

---
+++ test/models/launch/shop_conversion_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Launch
+  class ShopConversionTest < ActiveSupport::TestCase
+    test 'the truth' do
+      assert true
+    end
+  end
+end
--- test/models/shop_conversion_test.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ShopConversionTest < ActiveSupport::TestCase
-  test 'the truth' do
-    assert true
-  end
-end
